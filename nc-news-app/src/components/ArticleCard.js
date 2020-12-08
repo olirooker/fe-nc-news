9d1f82e51984c3290471b3765860f4d119d673f6
Oli Rooker <oliver.rooker@gmail.com>
2020-12-08 18:14:05 +0000

    date refactor using moment dependency on articles

--- nc-news-app/src/components/ArticleCard.js
+++ nc-news-app/src/components/ArticleCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import moment from 'moment';
 
 const ArticleCard = ({ articleData }) => {
+    // console.log(moment(articleData.created_at).fromNow())
+    // console.log(articleData.body)
     return (
         <li>
             <div>
-                <p><Link to={`/${articleData.topic}/articles`}>{articleData.topic}</Link>. Posted by <Link to={`/users/${articleData.author}/articles`}>{articleData.author}</Link> at {articleData.created_at}</p>
+                <p><Link to={`/${articleData.topic}/articles`}>{articleData.topic}</Link>. Posted by <Link to={`/users/${articleData.author}/articles`}>{articleData.author}</Link> at {moment(articleData.created_at).fromNow()}</p>
                 <p>{articleData.votes}</p>
             </div>
 
