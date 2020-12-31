4e7a3e61b989dcc5b8d094cd0a414b09e2646337
Oli Rooker <oliver.rooker@gmail.com>
2020-12-31 12:28:34 +0000

    add sort articles by comment count

--- nc-news-app/src/api.js
+++ nc-news-app/src/api.js
@@ -1,93 +1,91 @@
-import axios from 'axios';
+import axios from "axios";
 
 const ncNewsApi = axios.create({
-    // baseURL: 'https://ncoders-news-api.herokuapp.com/api',
-    baseURL: 'https://olis-nc-news-api.herokuapp.com/api',
+  // baseURL: 'https://ncoders-news-api.herokuapp.com/api',
+  baseURL: "https://olis-nc-news-api.herokuapp.com/api",
 });
 
 export const getTopics = () => {
-    return ncNewsApi.get('/topics')
-        .then(({ data }) => {
-            return data.topics
-        })
+  return ncNewsApi.get("/topics").then(({ data }) => {
+    return data.topics;
+  });
 };
 
 export const getArticles = (topic, username, order, sort_by) => {
-    return ncNewsApi.get('/articles', {
-        params: {
-            topic: topic,
-            username: username,
-            order,
-            sort_by,
-        }
+  return ncNewsApi
+    .get("/articles", {
+      params: {
+        topic: topic,
+        username: username,
+        order,
+        sort_by,
+      },
     })
-        .then(({ data }) => {
-            console.log(data)
-            return data.articles
-        })
+    .then(({ data }) => {
+      console.log(data);
+      return data.articles;
+    });
 };
 
 export const getSingleArticle = (article_id) => {
-    return ncNewsApi.get(`/articles/${article_id}`)
-        .then(({ data }) => {
-            return data.article
-        })
+  return ncNewsApi.get(`/articles/${article_id}`).then(({ data }) => {
+    return data.article;
+  });
 };
 
 export const getArticleComments = (article_id, order, sort_by) => {
-    return ncNewsApi.get(`/articles/${article_id}/comments`, {
-        params: {
-            order,
-            sort_by,
-        }
+  return ncNewsApi
+    .get(`/articles/${article_id}/comments`, {
+      params: {
+        order,
+        sort_by,
+      },
     })
-        .then(({ data }) => {
-            return data.comments
-        })
+    .then(({ data }) => {
+      return data.comments;
+    });
 };
 
 export const upVoteArticle = (article_id) => {
-    return ncNewsApi.patch(`/articles/${article_id}`, { inc_votes: 1 })
+  return ncNewsApi.patch(`/articles/${article_id}`, { inc_votes: 1 });
 };
 
 export const downVoteArticle = (article_id) => {
-    return ncNewsApi.patch(`/articles/${article_id}`, { inc_votes: -1 })
+  return ncNewsApi.patch(`/articles/${article_id}`, { inc_votes: -1 });
 };
 
 export const upVoteComment = (comment_id) => {
-    return ncNewsApi.patch(`/comments/${comment_id}`, { inc_votes: 1 })
+  return ncNewsApi.patch(`/comments/${comment_id}`, { inc_votes: 1 });
 };
 
 export const downVoteComment = (comment_id) => {
-    return ncNewsApi.patch(`/comments/${comment_id}`, { inc_votes: -1 })
+  return ncNewsApi.patch(`/comments/${comment_id}`, { inc_votes: -1 });
 };
 
 export const postComment = (newComment, article_id) => {
-    return ncNewsApi.post(`/articles/${article_id}/comments`, newComment)
-        .then(({ data }) => {
-            return data.comment
-        })
+  return ncNewsApi
+    .post(`/articles/${article_id}/comments`, newComment)
+    .then(({ data }) => {
+      return data.comment;
+    });
 };
 
 export const postArticle = (newArticle) => {
-    return ncNewsApi.post(`/articles`, newArticle)
-        .then(({ data }) => {
-            return data.article
-        })
+  return ncNewsApi.post(`/articles`, newArticle).then(({ data }) => {
+    return data.article;
+  });
 };
 
 export const deleteComment = (comment_id) => {
-    return ncNewsApi.delete(`/comments/${comment_id}`)
+  return ncNewsApi.delete(`/comments/${comment_id}`);
 };
 
 export const deleteArticle = (article_id) => {
-    return ncNewsApi.delete(`/articles/${article_id}`)
+  return ncNewsApi.delete(`/articles/${article_id}`);
 };
 
 export const getUser = (username) => {
-    return ncNewsApi.get(`/users/${username}`)
-        .then(({ data }) => {
-            // console.log(data)
-            return data.user
-        })
-}
+  return ncNewsApi.get(`/users/${username}`).then(({ data }) => {
+    return data.user;
+  });
+};
--- nc-news-app/src/components/Query.js
+++ nc-news-app/src/components/Query.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledQueries = styled.div`
-    margin: 1.5rem 0;
-    padding: 5px 10px;
-    background-color: white;
-    border: 1px solid lightgrey;
-    box-shadow: 3px 6px 8px #888888;
-    border-left: 5px solid yellowgreen;
-`
+  margin: 1.5rem 0;
+  padding: 5px 10px;
+  background-color: white;
+  border: 1px solid lightgrey;
+  box-shadow: 3px 6px 8px #888888;
+  border-left: 5px solid yellowgreen;
+`;
 
 const Query = ({ changeOrder, changeSort }) => {
-    const handleOrderChange = (event) => {
-        const newOrder = event.target.value;
-        changeOrder(newOrder);
-    };
+  const handleOrderChange = (event) => {
+    const newOrder = event.target.value;
+    changeOrder(newOrder);
+  };
 
-    const handleSortChange = (event) => {
-        const newSort = event.target.value;
-        changeSort(newSort);
-    };
+  const handleSortChange = (event) => {
+    const newSort = event.target.value;
+    changeSort(newSort);
+  };
 
-    return (
-        <StyledQueries>
-            <select defaultValue="" onChange={handleOrderChange}>
-                <option key="order-placeholder" disabled={true} value="">Order by:</option>
-                <option value="asc">asc</option>
-                <option value="desc">desc</option>
-            </select>
+  return (
+    <StyledQueries>
+      <select defaultValue="" onChange={handleOrderChange}>
+        <option key="order-placeholder" disabled={true} value="">
+          Order by:
+        </option>
+        <option value="asc">Asc</option>
+        <option value="desc">Desc</option>
+      </select>
 
-            <select defaultValue="" onChange={handleSortChange}>
-                <option key="sort-placeholder" disabled={true} value="">Sort by:</option>
-                <option value="votes">Votes</option>
-                <option value="created_at">Time</option>
-            </select>
-        </StyledQueries>
-    );
-}
+      <select defaultValue="" onChange={handleSortChange}>
+        <option key="sort-placeholder" disabled={true} value="">
+          Sort by:
+        </option>
+        <option value="votes">Votes</option>
+        <option value="created_at">Date Posted</option>
+        <option value="comment_count">Comments</option>
+      </select>
+    </StyledQueries>
+  );
+};
 
 export default Query;
