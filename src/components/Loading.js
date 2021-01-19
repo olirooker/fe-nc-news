15a1705c7644caf8ae09c09377b3d6cd407bf76e
Oli Rooker <oliver.rooker@gmail.com>
2021-01-19 16:59:01 +0000

    add loading component spinner from react loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
+import styled from 'styled-components';
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 80px;
+`;
 
 const Loading = () => {
-    return (
-        <div>
-            <p>Loading...</p>
-        </div>
-    )
+  return (
+    <LoadingContainer>
+      <ReactLoading type={'spin'} color={'#00A99D'} height={50} width={50} />
+    </LoadingContainer>
+  );
 };
-
 export default Loading;
