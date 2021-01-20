42767fac5683c15e841b3fafbf74422c47981f3f
Oli Rooker <oliver.rooker@gmail.com>
2021-01-20 15:57:48 +0000

    queries component styling

--- src/components/Query.js
+++ src/components/Query.js
@@ -1,13 +1,23 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const StyledQueries = styled.div`
-  margin: 1.5rem 0;
-  padding: 5px 10px;
+const Queries = styled.div`
+  margin: 1.8rem 0;
+  padding: 0.8rem;
   background-color: white;
-  border: 1px solid lightgrey;
-  box-shadow: 3px 6px 8px #888888;
-  border-left: 5px solid yellowgreen;
+  border: 1px solid #b5bdc4;
+  border-radius: 1rem;
+  box-shadow: 1.5px 3px 4px #888888;
+  display: flex;
+  justify-content: space-between;
+`;
+const DropDownSelect = styled.select`
+  padding: 0.5rem;
+  border-radius: 5px;
+  width: 49%;
+  font-size: 1.6rem;
+  cursor: pointer;
+  border-color: lightgrey;
 `;
 
 const Query = ({ changeOrder, changeSort }) => {
@@ -22,24 +32,24 @@ const Query = ({ changeOrder, changeSort }) => {
   };
 
   return (
-    <StyledQueries>
-      <select defaultValue="" onChange={handleOrderChange}>
-        <option key="order-placeholder" disabled={true} value="">
+    <Queries>
+      <DropDownSelect defaultValue='' onChange={handleOrderChange}>
+        <option key='order-placeholder' disabled={true} value=''>
           Order by:
         </option>
-        <option value="asc">Asc</option>
-        <option value="desc">Desc</option>
-      </select>
+        <option value='asc'>Asc</option>
+        <option value='desc'>Desc</option>
+      </DropDownSelect>
 
-      <select defaultValue="" onChange={handleSortChange}>
-        <option key="sort-placeholder" disabled={true} value="">
+      <DropDownSelect defaultValue='' onChange={handleSortChange}>
+        <option key='sort-placeholder' disabled={true} value=''>
           Sort by:
         </option>
-        <option value="votes">Votes</option>
-        <option value="created_at">Date Posted</option>
-        <option value="comment_count">Comments</option>
-      </select>
-    </StyledQueries>
+        <option value='votes'>Votes</option>
+        <option value='created_at'>Date Posted</option>
+        <option value='comment_count'>Comments</option>
+      </DropDownSelect>
+    </Queries>
   );
 };
 
