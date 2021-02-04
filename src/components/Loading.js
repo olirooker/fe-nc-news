13acbcf6c48daa95a3bbdc0cdfe98f2183ef6d67
Oli Rooker <oliver.rooker@gmail.com>
2021-02-04 16:18:54 +0000

    loading css module refactor and header fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
-import styled from 'styled-components';
-
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 80px;
-`;
+import loadingStyle from './styles/loading.module.css';
 
 const Loading = () => {
   return (
-    <LoadingContainer>
+    <div className={loadingStyle.loadingContainerPage}>
       <ReactLoading type={'spin'} color={'#00A99D'} height={50} width={50} />
-    </LoadingContainer>
+    </div>
   );
 };
 export default Loading;
--- src/components/LoadingButton.js
+++ src/components/LoadingButton.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
-import styled from 'styled-components';
-
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-left: auto;
-  margin-right: auto;
-`;
+import loadingStyle from './styles/loading.module.css';
 
 const LoadingButton = () => {
   return (
-    <LoadingContainer>
+    <div className={loadingStyle.loadingContainer}>
       <ReactLoading type={'spin'} color={'#0f326c'} height={20} width={20} />
-    </LoadingContainer>
+    </div>
   );
 };
 export default LoadingButton;
